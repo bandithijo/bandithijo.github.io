aff4b3ff75b177e9c4fe236b7e17f7765c382d54
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2022-12-14 23:38:47 +0800

    Update: Dark and Light ver.1

--- assets/js/headings-permalink.js
+++ assets/js/headings-permalink.js
@@ -2,7 +2,7 @@ var headings = document.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id]
 
 for (var i = 0; i < headings.length; i++) {
     headings[i].innerHTML =
-        '<a style="border-bottom:none;color:#000;" href="#' + headings[i].id + '">' +
+        '<a class="headings-permalink" style="border-bottom:none;" href="#' + headings[i].id + '">' +
             headings[i].innerText +
         '</a>';
 }
