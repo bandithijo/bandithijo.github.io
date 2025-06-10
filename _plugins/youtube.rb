6c1f699f79c92b4d6c641a27b783fb8cb18d4db8
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-06-10 14:20:15 +0800

    chore: modify jekyll custom tag for youtube

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -1,39 +1,27 @@
-# Jekyll - Easy Youtube Embed
-# Katie Harron - https://github.com/pibby
-#
-#   Input:
-#     {% youtube Al9FOtZcadQ %}
-#
-#   Output:
-#   <div class="video">
-#     <figure>
-#       <iframe width="640" height="480" src="//www.youtube.com/embed/Al9FOtZcadQ" allowfullscreen></iframe>
-#     </figure>
-#   </div>
-
 module Jekyll
-
   class Youtube < Liquid::Tag
-    @url = nil
-
     VIDEO_URL = /(\S+)/i
 
     def initialize(tag_name, markup, tokens)
       super
-
-      if markup =~ VIDEO_URL
-        @url = $1
-      end
+      @url = markup.strip.match(VIDEO_URL)[1]
     end
 
     def render(_context)
-      source = "<div class=\"\">"
-      source += "<iframe width=\"640\" height=\"480\" class=\"mx-auto\" src=\"/assets/images/bandithijo_logo.svg\" data-echo=\"//www.youtube.com/embed/#{@url}\" onerror=\"imgError(this);\" frameborder=\"0\" allowfullscreen></iframe>"
-      source += "</div>"
-      source
+      <<~HTML
+        <div class="relative w-full pb-[56.25%] h-0 overflow-hidden max-w-full">
+          <iframe
+            class="absolute top-0 left-0 w-full h-full lazy-loading"
+            src="/assets/images/bandithijo_logo.svg"
+            data-echo="https://www.youtube.com/embed/#{@url}"
+            frameborder="0"
+            allowfullscreen
+            onerror="imgError(this);"
+          ></iframe>
+        </div>
+      HTML
     end
-
-  end # Youtube
-end # Jekyll
+  end
+end
 
 Liquid::Template.register_tag('youtube', Jekyll::Youtube)
