388f13d38308443dee151d7aecab379775d0bd71
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-06-16 13:31:04 +0800

    chore: fix youtube tag

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -11,7 +11,7 @@ module Jekyll
       <<~HTML
         <div class="relative w-full pb-[56.25%] h-0 overflow-hidden max-w-full">
           <iframe
-            class="absolute top-0 left-0 w-full h-full lazy-loading"
+            class="absolute top-0 left-0 w-full h-full"
             src="/assets/images/bandithijo_logo.svg"
             data-echo="https://www.youtube.com/embed/#{@url}"
             frameborder="0"
